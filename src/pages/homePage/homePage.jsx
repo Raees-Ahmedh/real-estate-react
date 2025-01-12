eba5b07667fc40cc7346dd64081390ac244fd8fa
Raees Ahamed <raeesahmedh116@gmail.com>
2025-01-12 23:57:05 +0530

    nav bar completed

---
+++ src/pages/homePage/homePage.jsx
@@ -0,0 +1,11 @@
+import './homePage.scss'
+
+function HomePage() {
+    return (
+        <div className="homePage">
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default HomePage
